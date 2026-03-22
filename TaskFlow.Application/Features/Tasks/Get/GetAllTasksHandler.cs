22047c92db7b12887fafd88f05a4a0e361f8e027
serhatkuscu <srhtksc4@gmail.com>
2026-03-22 17:28:27 +0300

    implement  Get endpoint for tasks

---
+++ TaskFlow.Application/Features/Tasks/Get/GetAllTasksHandler.cs
@@ -0,0 +1,19 @@
+﻿using TaskFlow.Application.Interfaces.Repositories;
+using TaskFlow.Domain.Entities;
+
+namespace TaskFlow.Application.Features.Tasks.Get;
+
+public class GetAllTasksHandler
+{
+    private readonly ITaskRepository _taskRepository;
+
+    public GetAllTasksHandler(ITaskRepository taskRepository)
+    {
+        _taskRepository = taskRepository;
+    }
+
+    public async Task<List<TaskItem>> HandleAsync()
+    {
+        return await _taskRepository.GetAllAsync();
+    }
+}
--- TaskFlow.Application/Interfaces/Repositories/ITaskRepository.cs
+++ TaskFlow.Application/Interfaces/Repositories/ITaskRepository.cs
@@ -5,4 +5,5 @@ namespace TaskFlow.Application.Interfaces.Repositories;
 public interface ITaskRepository
 {
     Task AddAsync(TaskItem task);
+    Task<List<TaskItem>> GetAllAsync();
 }
